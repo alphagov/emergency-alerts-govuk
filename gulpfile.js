652fdc2da437ee0def66403031535cb10f0fa906
Tom Byers <tombaromba@gmail.com>
2021-02-23 15:59:24 +0000

    Add support for IE8
    Based on guidance from the design system team:
    
    https://frontend.design-system.service.gov.uk/supporting-ie8/#2-generate-an-ie8-specific-stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 'use strict';
 
+const path = require('path');
+
 const { src, pipe, dest, series, parallel, watch } = require('gulp');
 const Fiber = require('fibers');
 
 const plugins = {}
 plugins.sass = require('gulp-sass');
 plugins.gulpStylelint = require('gulp-stylelint');
+plugins.gulpif = require('gulp-if');
+plugins.postcss = require('gulp-postcss');
 
 const paths = {
   src: 'src/assets/',
   dist: 'dist/assets/',
+  node_modules: 'node_modules/',
   govuk_frontend: 'node_modules/govuk-frontend/'
 };
 
@@ -25,6 +30,10 @@ const copy = {
       return src(paths.govuk_frontend + 'govuk/assets/images/**/*')
         .pipe(dest(paths.dist + 'images/'));
     }
+  },
+  html5shiv: () => {
+    return src(paths.node_modules + 'html5shiv/dist/*.min.js')
+      .pipe(dest(paths.dist + 'javascripts/vendor/html5shiv/'));
   }
 };
 
@@ -47,6 +56,12 @@ const scss = {
           outputStyle: 'compressed'
         })
         .on('error', plugins.sass.logError))
+        .pipe(plugins.gulpif(
+          (file) => {
+            return path.basename(file.path) === 'main-ie8.css';
+          },
+          plugins.postcss([require('oldie')()])
+        ))
       .pipe(dest(paths.dist + 'stylesheets/'));
   }
 };
@@ -55,6 +70,7 @@ const defaultTask = parallel(
   parallel(
     copy.govuk_frontend.fonts,
     copy.govuk_frontend.images,
+    copy.html5shiv,
     scss.lint,
     scss.compile
   )
