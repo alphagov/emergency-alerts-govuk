cb6c15564437d34823d5da483779309141d93985
Tom Byers <tombaromba@gmail.com>
2021-05-14 12:42:45 +0100

    Tests datetimes in British Summer Time (BST)
    I am very new to this kind of work so have made
    some assumptions that I would like challenged:
    - I think that the datetime.datetime instance
      PyYAML produces from our iso8601 dates (which
      are in UTC) is not altered by being set to the
      UTC timezone
    - I have changed the first test, that tests each
      property of AlertsDate returns the format
      expected, to use a date outside of BST to dodge
      testing that capability
    - I have removed use of datetime.timezone to make
      things less confusing and match how we do it in
      the admin app
    - I am testing all I should be

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,8 @@
 import hashlib
-from datetime import datetime, timezone
+import pytz
+from datetime import datetime
 from pathlib import Path
+from pytz import timezone
 
 
 REPO = Path('.')
@@ -14,18 +16,23 @@ class AlertsDate(object):
 
     def __init__(self, _datetime):
         self._datetime = _datetime
+        self._local_datetime = _datetime.astimezone(timezone('Europe/London'))
 
     @property
     def as_lang(self, lang='en-GB'):
-        return '{dt.day} {dt:%B} {dt:%Y} at {dt:%H}:{dt:%M}'.format(dt=self._datetime)
+        return '{dt.day} {dt:%B} {dt:%Y} at {dt:%H}:{dt:%M}'.format(dt=self._local_datetime)
 
     @property
     def as_iso8601(self):
-        return self._datetime.isoformat()
+        return self._local_datetime.isoformat()
 
     @property
     def as_datetime(self):
         return self._datetime
+
+    @property
+    def as_local_datetime(self):
+        return self._local_datetime
 
 
 def file_fingerprint(path, root=DIST):
@@ -34,11 +41,11 @@ def file_fingerprint(path, root=DIST):
 
 
 def is_current_alert(alert):
-    now = datetime.now(timezone.utc)  # TODO: test this works with British Summer Time (BST)
+    now = datetime.now(pytz.utc)
 
     if alert['message_type'] == 'cancel':
         return False
-    if alert['expires'] <= now:  # pyyaml converts ISO 8601 dates to datetime.datetime instances
+    if alert['expires'].astimezone(pytz.utc) <= now:  # pyyaml converts ISO 8601 dates to datetime.datetime instances
         return False
     return True
 
