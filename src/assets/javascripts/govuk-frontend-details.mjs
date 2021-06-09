e9277e2204d723869d21d4c830a0d6cc5f4c43d6
Tom Byers <tombaromba@gmail.com>
2021-06-09 15:35:49 +0100

    Return GOVUK details JS to use nodeListForEach
    The change away from it created a discussion that
    is blocking the PR and the gains are marginal so
    this reverts it to unblock.

--- src/assets/javascripts/govuk-frontend-details.mjs
+++ src/assets/javascripts/govuk-frontend-details.mjs
@@ -1,11 +1,10 @@
+import { nodeListForEach } from 'govuk-frontend/govuk/common'
 import Details from 'govuk-frontend/govuk/components/details/details'
 
 export default function govukFrontendDetails () {
   var details = document.querySelectorAll('[data-module="govuk-details"]')
-  var detailsLen = details.length
-  var idx
 
-  for (idx = 0; idx < detailsLen; idx++) {
-    new Details(details[idx]).init()
-  }
+  nodeListForEach(details, function (detail) {
+    new Details(detail).init()
+  })
 }
