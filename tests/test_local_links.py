4a0c3e071373caedc3ae420f1d3167a5cb006504
Tom Byers <tombaromba@gmail.com>
2021-06-11 12:42:54 +0100

    Test that all local links go to existing pages

---
+++ tests/test_local_links.py
@@ -0,0 +1,87 @@
+import re
+from datetime import datetime
+from glob import glob
+from pathlib import Path
+from urllib.parse import urlparse
+
+import pytest
+import pytz
+from bs4 import BeautifulSoup
+from freezegun import freeze_time
+
+from lib.alerts import Alerts
+
+
+def get_page_html(env, template_path, alert_data=None):
+    template = env.get_template(template_path)
+    if alert_data is not None:
+        content = template.render({'alert_data': alert_data})
+    else:
+        content = template.render()
+    return BeautifulSoup(content, 'html.parser')
+
+
+def get_local_route_from_template_path(template_path):
+    local_route = '/alerts/' + Path(template_path).stem
+    if local_route != '/alerts/index':
+        return local_route
+    else:  # index page is mapped to root of /alerts directory
+        return '/alerts'
+
+
+all_templates = glob('./src/*.html')
+all_templates_except_alerts = filter(lambda template: template != './src/alert.html', all_templates)
+local_routes_except_alerts = [
+    get_local_route_from_template_path(template_path)
+    for template_path in all_templates]
+
+
+def test_local_links_in_pages_with_no_alerts(env):
+    for template_path in all_templates_except_alerts:
+        html = get_page_html(env, re.sub(r'^\./{1}', '', template_path))
+        local_links = html.select("a[href^='/alerts']")
+
+        if len(local_links) == 0:
+            assert True
+            return
+
+        for link in local_links:
+            path = urlparse(link['href']).path
+            assert path in local_routes_except_alerts
+
+
+@pytest.mark.parametrize("alert_timings", [
+    {  # current alert
+        "start": datetime(2021, 4, 21, 11, 25, tzinfo=pytz.utc),
+        "sent": datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc),
+        "expires": datetime(2021, 4, 21, 12, 30, tzinfo=pytz.utc)
+    },
+    {  # past alert
+        "start": datetime(2021, 4, 20, 11, 25, tzinfo=pytz.utc),
+        "sent": datetime(2021, 4, 20, 11, 30, tzinfo=pytz.utc),
+        "expires": datetime(2021, 4, 20, 12, 30, tzinfo=pytz.utc)
+    }
+])
+@freeze_time(datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc))
+def test_local_links_in_pages_with_alerts(env, alert_dict, alert_timings):
+    alert_dict.update(alert_timings)
+    alerts_data = Alerts([alert_dict])
+
+    # The file_fingerprint filter accesses the filesystem so mock
+    env.filters['file_fingerprint'] = lambda path: path
+    env.globals['alerts'] = [alerts_data]
+
+    # add route for an alert
+    local_routes = local_routes_except_alerts + ['/alerts/21-Apr-2021']
+
+    for template_path in all_templates:
+        html = get_page_html(env, re.sub(r'^\./{1}', '', template_path), alerts_data[0])
+        local_links = html.select("a[href^='/alerts']")
+
+        if len(local_links) == 0:
+            assert True
+            return
+
+        for link in local_links:
+            path = urlparse(link['href']).path
+            assert path in local_routes
