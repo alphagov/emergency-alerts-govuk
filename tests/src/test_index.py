52a97780893398e95b5c74e1b61fab8c193f54d2
Tom Byers <tombaromba@gmail.com>
2021-06-11 15:20:34 +0100

    Move the Jinja env used in tests into a fixture
    Follows the logic in this comment to make it more
    convenient and allows us to easily test with
    alerts data in the future:
    
    https://github.com/alphagov/notifications-govuk-alerts/pull/91#discussion_r649088444

--- tests/src/test_index.py
+++ tests/src/test_index.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup
 
-from tests.src import env
 
-
-def test_index_page():
+def test_index_page(env):
     template = env.get_template("src/index.html")
     content = template.render()
     html = BeautifulSoup(content, 'html.parser')
