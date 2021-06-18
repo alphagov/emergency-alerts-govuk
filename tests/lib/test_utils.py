bbb2762c7f1892343a2f3de79f6ac3560f5b6d84
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2021-06-18 13:45:58 +0100

    Fix file_fingerprint util test to assert filepath
    Signed-off-by: Richard Baker <richard.baker@digital.cabinet-office.gov.uk>

--- tests/lib/test_utils.py
+++ tests/lib/test_utils.py
@@ -7,7 +7,7 @@ from lib.utils import file_fingerprint, paragraphize
 
 def test_file_fingerprint_adds_hash_to_file_path():
     new_path = file_fingerprint('/tests/test_files/example.txt', root=Path('.'))
-    assert new_path == '/tests/test_files/example.txt?4d93d51945b88325c213640ef59fc50b'
+    assert new_path == '/tests/test_files/example-4d93d51945b88325c213640ef59fc50b.txt'
 
 
 def test_paragraphize_converts_newlines_to_paragraphs():
