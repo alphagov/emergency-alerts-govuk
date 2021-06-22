a96259f0f4333cde04ac07ebdb120882a34417fe
sakisv <me@sakisv.net>
2021-06-22 18:01:04 +0300

    Rewrite main.py to add Flask
    This will remove the previous functionality that we could display the
    alerts locally. Instead it adds a single endpoint `refresh-alerts` that
    will build, upload to s3 and purge the fastly cache in a single call.
    
    For now, for the purposes of this spike, the content of the alerts comes
    from the `data.yml` file, but we should implement a similar endpoint in
    notify-api to provide such data.

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-import http.server
+import hmac
+import logging
 import os
 
-PORT = 8000
+from flask import Blueprint, Flask, current_app, jsonify
+from flask_httpauth import HTTPBasicAuth
 
-web_dir = os.path.join(os.path.dirname(__file__), 'dist')
-os.chdir(web_dir)
+from build import alerts_from_yaml, get_rendered_pages
+from lib.utils import upload_to_s3, purge_cache
 
-Handler = http.server.SimpleHTTPRequestHandler
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+main = Blueprint('main', __name__)
+auth = HTTPBasicAuth()
 
-Handler.extensions_map = {
-    '': 'text/html',
-    '.css': 'text/css'
+credentials = {
+    "notify-api": os.getenv('NOTIFY_API_PASSWORD', 'TestPass')
 }
 
-httpd = http.server.HTTPServer(("", PORT), Handler)
 
-print(f"serving at http://localhost:{PORT}")
-httpd.serve_forever()
+@auth.verify_password
+def verify_password(username, password):
+    credentials = current_app.config['CREDENTIALS']
+    if username in credentials and hmac.compare_digest(credentials[username], password):
+        return username
+
+
+def _get_current_alerts():
+    return alerts_from_yaml()
+
+
+@main.route('/refresh-alerts', methods=['POST'])
+# @auth.login_required
+def new_alert():
+    current_alerts = _get_current_alerts()
+    rendered_pages = get_rendered_pages(current_alerts)
+
+    upload_to_s3(rendered_pages)
+
+    purge_cache()
+    return jsonify(success=True)
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main)
+
+    return app
+
+
+app = create_app()
