71aa334cf2cfc73810873accd50129fc760f8cf8
sakisv <me@sakisv.net>
2021-07-12 14:46:18 +0300

    Add function to restore local development
    The previous approach had to hack around the filesystem limitations,
    that would not allow having both a file and a directory sharing the same
    name (i.e. "alerts")
    
    This takes a more dynamic approach where it reuses the existing dict
    which contains the rendered content and serves it to the user.
    
    This is only available in the development environment and it relies on
    path manipulation to serve static content from flask.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import hmac
 import logging
 import os
 
-from flask import Blueprint, Flask, current_app, jsonify
+from flask import Blueprint, Flask, current_app, jsonify, send_from_directory
 from flask_httpauth import HTTPBasicAuth
 
-from build import alerts_from_api, get_rendered_pages
+from build import alerts_from_yaml, alerts_from_api, get_rendered_pages
 from lib.utils import purge_cache, upload_to_s3
 
 logger = logging.getLogger()
@@ -35,6 +35,26 @@ def refresh_alerts():
 
     purge_cache()
     return jsonify(success=True)
+
+
+@main.route('/<path:key>', methods=['GET'])
+def show_page(key):
+    if os.getenv("FLASK_ENV") != "development":
+        return "not found", 404
+
+    alerts = alerts_from_yaml()
+    rendered_pages = get_rendered_pages(alerts)
+
+    if key not in rendered_pages:
+
+        if key.startswith("alerts/assets"):
+            key = key.replace("alerts/assets", "dist/alerts/assets")
+            dirname, filename = key.rsplit("/", 1)
+            return send_from_directory(dirname, filename)
+
+        return "not found", 404
+
+    return rendered_pages[key]
 
 
 def create_app():
