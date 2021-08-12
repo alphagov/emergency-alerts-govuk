e1ca26a7b286ea922058c282d22344ccc2fe54bf
sakisv <me@sakisv.net>
2021-08-12 18:47:58 +0300

    Implement celery task to render upload and flush content
    This is fairly straight forward:
    
    - Get all the alerts content
    - Render pages based on it
    - Upload the pages to S3
    - Flush fastly cache

---
+++ app/celery/tasks.py
@@ -0,0 +1,20 @@
+from app import notify_celery
+from app.render import alerts_from_yaml, get_rendered_pages
+from lib.utils import purge_cache, upload_to_s3
+
+
+@notify_celery.task(bind=True, name="publish-govuk-alerts", max_retries=20, retry_backoff=True, retry_backoff_max=300)
+def publish_govuk_alerts(self):
+    from flask import current_app
+
+    try:
+        alerts = alerts_from_yaml()
+        rendered_pages = get_rendered_pages(alerts)
+
+        upload_to_s3(current_app.config, rendered_pages)
+
+        purge_cache(current_app.config)
+    except Exception:
+        current_app.logger.exception("Failed to publish content to gov.uk/alerts: " + Exception)
+        self.retry(queue=current_app.config['QUEUE_NAME'])
+        return
---
+++ tests/app/celery/test_tasks.py
@@ -0,0 +1,20 @@
+from unittest.mock import ANY, patch
+
+from app.celery.tasks import publish_govuk_alerts
+
+
+@patch('app.celery.tasks.alerts_from_yaml')
+@patch('app.celery.tasks.get_rendered_pages')
+@patch('app.celery.tasks.upload_to_s3')
+@patch('app.celery.tasks.purge_cache')
+def test_publish_govuk_alerts(
+    mock_purge_cache,
+    mock_upload_to_s3,
+    mock_get_rendered_pages,
+    mock_alerts_from_yaml,
+):
+    publish_govuk_alerts()
+    mock_alerts_from_yaml.assert_called_once()
+    mock_get_rendered_pages.assert_called_once_with(mock_alerts_from_yaml.return_value)
+    mock_upload_to_s3.assert_called_once_with(ANY, mock_get_rendered_pages.return_value)
+    mock_purge_cache.assert_called_once()
