b05d6749a0453962cd72659f2f17853c36af53ea
Tom Byers <tombaromba@gmail.com>
2021-08-12 13:17:30 +0100

    Fix bugs in test_all_pages.py
    The tests in test_all_pages.py had a few mistakes
    in their code which meant they ran green
    incorrectly. The mistakes were:
    
    In
    
    test_local_links_lead_to_existing_routes_in_pages_with_alerts,
    
    The alerts data instance was being passed to the ninja
    environment wrapped in a list, which doesn't
    happen in the build.
    
    In
    
    test_all_pages_with_details_in_have_the_js_for_it,
    
    The data used to make the alert added current or
    past mutated the alert in a loop so all of it was
    applied at the same time.
    
    This fixes those issues.

--- tests/test_all_pages.py
+++ tests/test_all_pages.py
@@ -56,13 +56,15 @@ def test_local_links_lead_to_existing_routes_in_pages_with_no_alerts(env):
 ])
 @freeze_time(datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc))
 def test_local_links_lead_to_existing_routes_in_pages_with_alerts(env, alert_dict, alert_timings):
+
+    # fake an alert existing in the routes and data
+    local_routes = local_routes_except_alerts + ['/alerts/21-Apr-2021']
+    alert_dict['identifier'] = '21-Apr-2021'
+
     alert_dict.update(alert_timings)
     alerts_data = Alerts([alert_dict])
 
-    env.globals['alerts'] = [alerts_data]
-
-    # add route for an alert
-    local_routes = local_routes_except_alerts + ['/alerts/21-Apr-2021']
+    env.globals['alerts'] = alerts_data
 
     for template_path in all_templates:
         html = render_template(
@@ -92,28 +94,26 @@ def test_links_have_correct_class_attribute(env, alert_dict, template_path):
         assert 'govuk-link' in link['class']
 
 
+@pytest.mark.parametrize("alert_timings", [
+    {  # current alert
+        "starts_at": datetime(2021, 4, 21, 11, 25, tzinfo=pytz.utc),
+        "approved_at": datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc),
+        "cancelled_at": datetime(2021, 4, 21, 12, 30, tzinfo=pytz.utc)
+    },
+    {  # past alert
+        "starts_at": datetime(2021, 4, 20, 11, 25, tzinfo=pytz.utc),
+        "approved_at": datetime(2021, 4, 20, 11, 30, tzinfo=pytz.utc),
+        "cancelled_at": datetime(2021, 4, 20, 12, 30, tzinfo=pytz.utc)
+    }
+])
 @pytest.mark.parametrize('template_path', all_templates)
 @freeze_time(datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc))
-def test_all_pages_with_details_in_have_the_js_for_it(env, alert_dict, template_path):
-    current_and_past_alert_timings = [
-        {  # current alert
-            "starts_at": datetime(2021, 4, 21, 11, 25, tzinfo=pytz.utc),
-            "approved_at": datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc),
-            "cancelled_at": datetime(2021, 4, 21, 12, 30, tzinfo=pytz.utc)
-        },
-        {  # past alert
-            "starts_at": datetime(2021, 4, 20, 11, 25, tzinfo=pytz.utc),
-            "approved_at": datetime(2021, 4, 20, 11, 30, tzinfo=pytz.utc),
-            "cancelled_at": datetime(2021, 4, 20, 12, 30, tzinfo=pytz.utc)
-        }
-    ]
-
-    # add current and past alerts to ensure all pages exist and have content
-    for alert_timings in current_and_past_alert_timings:
-        alert_dict.update(alert_timings)
-
+def test_all_pages_with_details_in_have_the_js_for_it(env, alert_dict, alert_timings, template_path):
+    alert_dict.update(alert_timings)
     alerts_data = Alerts([alert_dict])
+
     env.globals['alerts'] = [alerts_data]
+
     html = render_template(
         env,
         re.sub(r'^\./{1}', '', template_path),
