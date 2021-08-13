e0539128b3eacd1faae67a8faf37d63408fcfcf7
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-08-13 16:06:47 +0100

    Remove unnecessary conditional in main.py
    Resolves https://github.com/alphagov/notifications-govuk-alerts/pull/151#discussion_r688421655

--- write_to_disk.py
+++ write_to_disk.py
@@ -1,18 +1,12 @@
 from app.render import alerts_from_yaml, get_rendered_pages
 
+alerts = alerts_from_yaml()
+rendered_pages = get_rendered_pages(alerts)
 
-def write_to_disk():
-    alerts = alerts_from_yaml()
-    rendered_pages = get_rendered_pages(alerts)
+for filename, content in rendered_pages.items():
+    target = filename
+    if filename == "alerts":
+        target = "index.html"
 
-    for filename, content in rendered_pages.items():
-        target = filename
-        if filename == "alerts":
-            target = "index.html"
-
-        with open(f"dist/{target}", "w") as fp:
-            fp.write(content)
-
-
-if __name__ == "__main__":
-    write_to_disk()
+    with open(f"dist/{target}", "w") as fp:
+        fp.write(content)
