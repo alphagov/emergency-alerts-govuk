ab16e876eec42873a6089ec609dcdeca07737aa6
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-08-23 17:19:09 +0100

    Extract single Flask view into own blueprint
    This matches the global blueprint style and naming that we have in
    our other repos [1][2].
    
    [1]: https://github.com/alphagov/notifications-admin/blob/master/app/main/__init__.py
    [2]: https://github.com/alphagov/document-download-frontend/blob/master/app/main/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ from notifications_utils import logging
 from notifications_utils.clients.statsd.statsd_client import StatsdClient
 
 from app.celery.celery import NotifyCelery
-from app.models.alerts import Alerts
-from app.render import get_rendered_pages
 
 notify_celery = NotifyCelery()
 statsd_client = StatsdClient()
@@ -22,21 +20,14 @@ def create_app():
     environment = os.getenv('NOTIFY_ENVIRONMENT', 'development')
     application.config.from_object(configs[environment])
 
+    from app.main import bp as main
+    application.register_blueprint(main)
+
     from app.commands import setup_commands
     setup_commands(application)
 
     statsd_client.init_app(application)
     logging.init_app(application, statsd_client)
     notify_celery.init_app(application)
-
-    @application.route('/<path:key>', methods=['GET'])
-    def show_page(key):
-        alerts = Alerts.load()
-        rendered_pages = get_rendered_pages(alerts)
-
-        if key in rendered_pages:
-            return rendered_pages[key]
-
-        return application.send_static_file(key)
 
     return application
