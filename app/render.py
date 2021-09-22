dd21501176d83544aab6aa8c460651bca61ba829
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2021-09-22 17:14:05 +0100

    add get_url_for_alert function
    this function figures out the url for a given alert, based on the
    alert's order in the day. Alerts are sorted by starts_at and then by id
    to split ties.
    
    If it's the first (or only) public alert in a day, we return the date
    formatted as '3-jun-2021'. If it's the second or later public alert in
    the day we give it a 1-indexed suffix, so starting at '3-jun-2021-2' and
    counting up from there

--- app/render.py
+++ app/render.py
@@ -17,6 +17,35 @@ all_view_paths = [
 ]
 
 
+def get_url_for_alert(alert, alerts):
+    """
+    Gets the url slug for an alert (given the global alerts object obtained from `Alerts.load()`.)
+
+    Note: The alert must be public to have a url slug.
+
+    Will return a date string in the style '3-jun-2021', unless there were already alerts that day, in which case it'll
+    append a 1-indexed counter on to the end eg '3-jun-2021-2'. Note that the first alert of the day never has a count.
+    """
+    alerts_on_this_day = sorted([
+        other_alert
+        for other_alert in alerts.public
+        if other_alert.starts_at_date.as_local_date == alert.starts_at_date.as_local_date
+    ])
+
+    if not alerts_on_this_day:
+        raise ValueError(f'Alert {alert.id} is not public so does not have a URL')
+
+    # if this is the first alert of the day (or the only alert of the day), then don't include a counter
+    if alert == alerts_on_this_day[0]:
+        return alert.starts_at_date.as_url
+
+    # count through the alerts that day til we find this one, so we know
+    for i, other_alert in enumerate(alerts_on_this_day[1:], start=2):
+        if alert == other_alert:
+            return f'{alert.starts_at_date.as_url}-{i}'
+    raise ValueError(f'Couldnt find alert {alert.id} in public alerts for day')
+
+
 def setup_jinja_environment(alerts):
     jinja_loader = ChoiceLoader([
         FileSystemLoader(str(TEMPLATES)),
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,20 +9,36 @@ from app import create_app
 from app.models.alerts import Alerts
 
 
+def create_alert_dict(
+    *,
+    id=None,
+    headline=None,
+    content=None,
+    areas=None,
+    channel=None,
+    identifier=None,
+    starts_at=None,
+    approved_at=None,
+    cancelled_at=None,
+    finishes_at=None,
+):
+    return {
+        'id': str(id or uuid.UUID(int=0)),
+        'headline': headline or 'Emergency alert',
+        'content': content or 'Something',
+        'areas': areas or dict(),
+        'channel': channel or 'severe',
+        'identifier': identifier or '1234',
+        'starts_at': starts_at or datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc),
+        'approved_at': approved_at or datetime(2021, 4, 21, 11, 25, tzinfo=pytz.utc),
+        'cancelled_at': cancelled_at or datetime(2021, 4, 21, 12, 30, tzinfo=pytz.utc),
+        'finishes_at': finishes_at or datetime(2021, 4, 21, 15, 30, tzinfo=pytz.utc)
+    }
+
+
 @pytest.fixture()
 def alert_dict():
-    return {
-        'id': str(uuid.UUID(int=0)),
-        'headline': 'Emergency alert',
-        'content': 'Something',
-        'areas': dict(),
-        'channel': 'severe',
-        'identifier': '1234',
-        'starts_at': datetime(2021, 4, 21, 11, 30, tzinfo=pytz.utc),
-        'approved_at': datetime(2021, 4, 21, 11, 25, tzinfo=pytz.utc),
-        'cancelled_at': datetime(2021, 4, 21, 12, 30, tzinfo=pytz.utc),
-        'finishes_at': datetime(2021, 4, 21, 15, 30, tzinfo=pytz.utc)
-    }
+    return create_alert_dict()
 
 
 @pytest.fixture(scope='session')
