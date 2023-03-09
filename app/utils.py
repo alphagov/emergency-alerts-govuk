44186d5f9d85ae337614d5136f6654808cbe739b
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-03-09 17:37:56 +0000

    add session token

--- app/utils.py
+++ app/utils.py
@@ -59,6 +59,7 @@ def upload_to_s3(rendered_pages):
     session = boto3.session.Session(
         aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        aws_session_token=os.environ.get("AWS_SESSION_TOKEN"),
         region_name=os.environ.get("AWS_REGION"),
     )
 
