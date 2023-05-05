bca110efd6c8d62eafc5f850a4ce2cbbdb11f2b9
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-05-05 11:35:06 +0100

    use double-quotes throughout for consistency of style

--- app/config.py
+++ app/config.py
@@ -8,21 +8,21 @@ class Config():
     NOTIFICATION_QUEUE_PREFIX = f"{os.getenv('ENVIRONMENT')}-"
     QUEUE_NAME = "govuk-alerts"
 
-    NOTIFY_APP_NAME = 'govuk-alerts'
-    AWS_REGION = 'eu-west-2'
-    NOTIFY_LOG_PATH = os.getenv('NOTIFY_LOG_PATH', 'application.log')
+    NOTIFY_APP_NAME = "govuk-alerts"
+    AWS_REGION = "eu-west-2"
+    NOTIFY_LOG_PATH = os.getenv("NOTIFY_LOG_PATH", "application.log")
 
     BROADCASTS_AWS_ACCESS_KEY_ID = os.getenv("BROADCASTS_AWS_ACCESS_KEY_ID")
     BROADCASTS_AWS_SECRET_ACCESS_KEY = os.getenv("BROADCASTS_AWS_SECRET_ACCESS_KEY")
-    BROADCASTS_AWS_REGION = 'eu-west-2'
+    BROADCASTS_AWS_REGION = "eu-west-2"
     GOVUK_ALERTS_S3_BUCKET_NAME = os.getenv("GOVUK_ALERTS_S3_BUCKET_NAME")
 
     FASTLY_SERVICE_ID = os.getenv("FASTLY_SERVICE_ID")
     FASTLY_API_KEY = os.getenv("FASTLY_API_KEY")
-    FASTLY_SURROGATE_KEY = 'notify-emergency-alerts'
+    FASTLY_SURROGATE_KEY = "notify-emergency-alerts"
 
-    NOTIFY_API_HOST_NAME = os.environ.get('NOTIFY_API_HOST_NAME')
-    NOTIFY_API_CLIENT_SECRET = os.environ.get('NOTIFY_API_CLIENT_SECRET')
+    NOTIFY_API_HOST_NAME = os.environ.get("NOTIFY_API_HOST_NAME")
+    NOTIFY_API_CLIENT_SECRET = os.environ.get("NOTIFY_API_CLIENT_SECRET")
     NOTIFY_API_CLIENT_ID = "govuk-alerts"
 
     CELERY = {
@@ -36,17 +36,17 @@ class Config():
             "task_acks_late": True,
         },
         "timezone": "UTC",
-        'imports': ['app.celery.tasks'],
-        'task_queues': [
-            Queue(QUEUE_NAME, Exchange('default'), routing_key=QUEUE_NAME)
+        "imports": ["app.celery.tasks"],
+        "task_queues": [
+            Queue(QUEUE_NAME, Exchange("default"), routing_key=QUEUE_NAME)
         ],
         # Restart workers after a few tasks have been executed - this will help prevent any memory leaks
         # (not that we should be encouraging sloppy memory management). Although the tasks are time-critical,
         # we don't expect to get them in quick succession, so a small restart delay is acceptable.
-        'worker_max_tasks_per_child': 10
+        "worker_max_tasks_per_child": 10
     }
 
-    STATSD_HOST = os.getenv('STATSD_HOST')
+    STATSD_HOST = os.getenv("STATSD_HOST")
     STATSD_PORT = 8125
     STATSD_ENABLED = bool(STATSD_HOST)
 
@@ -81,9 +81,9 @@ class Preview(Config):
 
 
 configs = {
-    'development': Development,
-    'test': Test,
-    'staging': Staging,
-    'preview': Preview,
-    'production': Config,
+    "development": Development,
+    "test": Test,
+    "staging": Staging,
+    "preview": Preview,
+    "production": Config,
 }
