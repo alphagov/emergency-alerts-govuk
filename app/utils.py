26902929353b20c7f435f30b340735361b9fcd44
Jake <jake.wych@digital.cabinet-office.gov.uk>
2023-05-12 11:27:48 +0100

    adding condition to differentiate decoupled or local

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,5 @@
 import os
 import re
-import sys
 from pathlib import Path
 
 import boto3
@@ -51,8 +50,18 @@ def is_in_uk(simple_polygons):
 
 
 def upload_html_to_s3(rendered_pages):
-    sys.stdout.write(sys.version)
-    session = boto3.Session()
+    notify_environment = os.environ.get('NOTIFY_ENVIRONMENT')
+
+    if (notify_environment == "decoupled"):
+        session = boto3.Session()
+
+    else:
+        session = boto3.Session(
+            aws_access_key_id=current_app.config["BROADCASTS_AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=current_app.config["BROADCASTS_AWS_SECRET_ACCESS_KEY"],
+            region_name=current_app.config["BROADCASTS_AWS_REGION"],
+        )
+
     s3 = session.resource('s3')
 
     bucket_name = os.environ.get('GOVUK_ALERTS_S3_BUCKET_NAME', "test-bucket")
@@ -69,7 +78,17 @@ def upload_assets_to_s3():
 
     assets = get_assets(DIST)
 
-    session = boto3.Session()
+    notify_environment = os.environ.get('NOTIFY_ENVIRONMENT')
+
+    if (notify_environment == "decoupled"):
+        session = boto3.Session()
+
+    else:
+        session = boto3.Session(
+            aws_access_key_id=current_app.config["BROADCASTS_AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=current_app.config["BROADCASTS_AWS_SECRET_ACCESS_KEY"],
+            region_name=current_app.config["BROADCASTS_AWS_REGION"],
+        )
     s3 = session.resource('s3')
 
     bucket_name = os.environ.get('GOVUK_ALERTS_S3_BUCKET_NAME', "test-bucket")
