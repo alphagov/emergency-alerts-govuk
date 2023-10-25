d8aeb1a0d50b42ee598bc739c56a66e7e1b15cdb
Jake <jake.wych@digital.cabinet-office.gov.uk>
2023-10-25 14:05:52 +0100

    debugging veriable names

--- .codepipeline/deploy-service.sh
+++ .codepipeline/deploy-service.sh
@@ -15,7 +15,8 @@ function update_task_defintion(){
         echo "SERVICE is required."
         exit
     fi;
-    
+    echo $SERVICE_NAME
+    echo $CLUSTER_NAME
     echo "=============== GETTING LATEST TASK DEFINITION ==============="
     latest_task_def=$(aws ecs list-task-definitions \
         --status ACTIVE \
