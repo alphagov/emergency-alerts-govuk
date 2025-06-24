355f37bcf3a49a3c83ea4c24bc8b889295e60803
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-06-24 10:36:12 +0100

    Provide the correct MIME type for Atom/XML in local (#394)

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, Response, current_app
 
 from app.models.alerts import Alerts
 from app.render import get_rendered_pages
 
-bp = Blueprint('main', __name__)
+bp = Blueprint("main", __name__)
 
 
-@bp.route('/<path:key>', methods=['GET'])
+@bp.route("/<path:key>", methods=["GET"])
 def show_page(key):
     alerts = Alerts.load()
     rendered_pages = get_rendered_pages(alerts)
 
     if key in rendered_pages:
-        return rendered_pages[key]
+        if key.endswith(".atom") or key.endswith(".xsl"):
+            return Response(rendered_pages[key], mimetype="text/xml")
+        else:
+            return rendered_pages[key]
 
     return current_app.send_static_file(key)
