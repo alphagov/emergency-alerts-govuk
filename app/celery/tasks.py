18412135d91656b8d50f7bf1017df83ebf4899c4
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-06-24 11:48:22 +0100

    Use TaskNames constant

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -2,13 +2,20 @@ import time
 
 from flask import current_app
 
+from emergency_alerts_utils.celery import TaskNames
 from app import notify_celery
 from app.models.alerts import Alerts
 from app.render import get_rendered_pages
 from app.utils import purge_fastly_cache, upload_html_to_s3
 
 
-@notify_celery.task(bind=True, name="publish-govuk-alerts", max_retries=20, retry_backoff=True, retry_backoff_max=300)
+@notify_celery.task(
+    bind=True,
+    name=TaskNames.PUBLISH_GOVUK_ALERTS,
+    max_retries=20,
+    retry_backoff=True,
+    retry_backoff_max=300,
+)
 def publish_govuk_alerts(self, broadcast_event_id=""):
     try:
         alerts = Alerts.load()
@@ -18,14 +25,16 @@ def publish_govuk_alerts(self, broadcast_event_id=""):
         purge_fastly_cache()
     except Exception:
         current_app.logger.exception("Failed to publish content to gov.uk/alerts")
-        self.retry(queue=current_app.config['QUEUE_NAME'])
+        self.retry(queue=current_app.config["QUEUE_NAME"])
 
 
 @notify_celery.task(name="trigger-govuk-alerts-healthcheck")
 def trigger_govuk_alerts_healthcheck():
     try:
         time_stamp = int(time.time())
-        with open("/eas/emergency-alerts-govuk/celery-beat-healthcheck", mode="w") as file:
+        with open(
+            "/eas/emergency-alerts-govuk/celery-beat-healthcheck", mode="w"
+        ) as file:
             file.write(str(time_stamp))
     except Exception:
         current_app.logger.exception("Unable to generate health-check timestamp")
