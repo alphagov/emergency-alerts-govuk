c2aefc826743cb342fdcee1422adab9b91051447
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-07-07 12:05:49 +0100

    Add acknowledgement endpoint to API

--- app/notify_client/alerts_api_client.py
+++ app/notify_client/alerts_api_client.py
@@ -3,23 +3,18 @@ from notifications_python_client.base import BaseAPIClient
 
 
 class AlertsApiClient(BaseAPIClient):
-    TIMESTAMP_FIELDS = [
-        'approved_at',
-        'starts_at',
-        'cancelled_at',
-        'finishes_at'
-    ]
+    TIMESTAMP_FIELDS = ["approved_at", "starts_at", "cancelled_at", "finishes_at"]
 
     def __init__(self):
         super().__init__("a" * 73, "b")
 
     def init_app(self, app):
-        self.base_url = app.config['NOTIFY_API_HOST_NAME']
-        self.api_key = app.config['NOTIFY_API_CLIENT_SECRET']
-        self.service_id = app.config['NOTIFY_API_CLIENT_ID']
+        self.base_url = app.config["NOTIFY_API_HOST_NAME"]
+        self.api_key = app.config["NOTIFY_API_CLIENT_SECRET"]
+        self.service_id = app.config["NOTIFY_API_CLIENT_ID"]
 
     def get_alerts(self):
-        data = self.get(url='/govuk-alerts')['alerts']
+        data = self.get(url="/govuk-alerts")["alerts"]
 
         for alert_dict in data:
             for field in self.TIMESTAMP_FIELDS:
@@ -27,3 +22,9 @@ class AlertsApiClient(BaseAPIClient):
                     alert_dict[field] = dt_parse(alert_dict[field])
 
         return data
+
+    def send_publish_acknowledgement(self):
+        return self.post(url="/govuk-alerts/acknowledge")
+
+
+alerts_api_client = AlertsApiClient()
