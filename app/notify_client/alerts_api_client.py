19ea1fd1ba7699d09853071f65b6e1f417d4ef75
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-07-08 10:56:30 +0100

    Undo auto format

--- app/notify_client/alerts_api_client.py
+++ app/notify_client/alerts_api_client.py
@@ -3,18 +3,23 @@ from notifications_python_client.base import BaseAPIClient
 
 
 class AlertsApiClient(BaseAPIClient):
-    TIMESTAMP_FIELDS = ["approved_at", "starts_at", "cancelled_at", "finishes_at"]
+    TIMESTAMP_FIELDS = [
+        'approved_at',
+        'starts_at',
+        'cancelled_at',
+        'finishes_at'
+    ]
 
     def __init__(self):
         super().__init__("a" * 73, "b")
 
     def init_app(self, app):
-        self.base_url = app.config["NOTIFY_API_HOST_NAME"]
-        self.api_key = app.config["NOTIFY_API_CLIENT_SECRET"]
-        self.service_id = app.config["NOTIFY_API_CLIENT_ID"]
+        self.base_url = app.config['NOTIFY_API_HOST_NAME']
+        self.api_key = app.config['NOTIFY_API_CLIENT_SECRET']
+        self.service_id = app.config['NOTIFY_API_CLIENT_ID']
 
     def get_alerts(self):
-        data = self.get(url="/govuk-alerts")["alerts"]
+        data = self.get(url='/govuk-alerts')['alerts']
 
         for alert_dict in data:
             for field in self.TIMESTAMP_FIELDS:
