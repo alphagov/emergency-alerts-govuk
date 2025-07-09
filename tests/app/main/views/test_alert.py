4e2efc1c33e897b2d05f64d11a6df2f5a5ddbf83
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-07-09 14:07:38 +0100

    Adds tests that assert extra content appears on alert pages

--- tests/app/main/views/test_alert.py
+++ tests/app/main/views/test_alert.py
@@ -5,6 +5,7 @@ from dateutil.parser import parse as dt_parse
 from freezegun import freeze_time
 
 from app.models.alerts import Alerts
+from tests import normalize_spaces
 from tests.conftest import create_alert_dict
 
 
@@ -31,16 +32,12 @@ def test_alert_links_to_correct_page_based_on_url_slug(is_expired, client_get, m
     mocker.patch('app.models.alert.Alert.is_expired', is_expired)
     mocker.patch('app.models.alerts.Alerts.load', return_value=Alerts([
         create_alert_dict(id=uuid4(), content='test 1', starts_at=dt_parse('2021-04-21T11:00:00Z')),
-        create_alert_dict(id=uuid4(), content='test 2', starts_at=dt_parse('2021-04-21T12:00:00Z'),
-                          extra_content="Test Extra Content"),
+        create_alert_dict(id=uuid4(), content='test 2', starts_at=dt_parse('2021-04-21T12:00:00Z')),
     ]))
 
     html = client_get('alerts/21-apr-2021-2')
     assert html.select('p.govuk-body-l')[0].text == 'test 2'
     assert html.select('p.govuk-body')[0].text.strip() == (
-        'Extra content: Test Extra Content'
-    )
-    assert html.select('p.govuk-body')[1].text.strip() == (
         'Sent by the UK government at 1:00pm on Wednesday 21 April 2021'
     )
 
@@ -105,3 +102,22 @@ def test_urls_in_alerts_are_clickable(client_get, mocker, url, expected_href_att
     link = html.select_one('p.govuk-body-l a.govuk-link')
     assert link['href'] == expected_href_attribute
     assert link.text == url
+
+
+@pytest.mark.parametrize('extra_content', (('TEST 1', 'Test 2')))
+def test_alert_displays_extra_content_if_exists(client_get, mocker, extra_content):
+    mocker.patch('app.models.alerts.Alerts.load', return_value=Alerts([
+        create_alert_dict(id=uuid4(), content='test 1', starts_at=dt_parse('2021-04-21T11:00:00Z'),
+                          extra_content=extra_content)]))
+
+    html = client_get('alerts/21-apr-2021')
+
+    assert html.select_one('#extra-content').text.strip() == f"Extra content: {extra_content}"
+
+
+def test_alert_no_extra_content(client_get, mocker):
+    mocker.patch('app.models.alerts.Alerts.load', return_value=Alerts([
+        create_alert_dict(id=uuid4(), content='test 1', starts_at=dt_parse('2021-04-21T11:00:00Z'))]))
+
+    html = client_get('alerts/21-apr-2021')
+    assert not html.select_one('#extra-content')
