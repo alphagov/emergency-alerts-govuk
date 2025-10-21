8879de64ded4dfaa29374c7e4a3abe6943408760
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-10-21 15:22:00 +0100

    script changes

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -2,15 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".local-time").forEach(function (element) {
     const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
     const locale = Intl.DateTimeFormat().resolvedOptions().locale;
-    var utcString = element.getAttribute("data-utc");
-    var utcDate = new Date(utcString);
+    const utcString = element.getAttribute("data-utc");
+    const utcDate = new Date(utcString);
     const localDate = utcDate.toLocaleString(locale, {
       timezone: tz,
       timeZoneName: "short",
     });
-
     // convert from "DD/MM/YYYY, HH:MM:SS TZ" to "YYYY-MM-DD HH:MM TZ"
-    displayDate = localDate.replace(
+    const displayDate = localDate.replace(
       /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}:\d{2}):\d{2} (\w+)/,
       "$3-$2-$1 $4 $5"
     );
