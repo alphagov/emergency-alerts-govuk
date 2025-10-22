620c4cd72e997c1fce8b8221cd8b480a133481ad
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-10-22 23:42:30 +0100

    revert gulpfile and modify feed javascript to try to get past linter

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".local-time").forEach(function (element) {
-    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const locale = Intl.DateTimeFormat().resolvedOptions().locale;
-    const datetimeString = element.getAttribute("data-datetime");
-    const datetimeObj = new Date(datetimeString);
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.local-time').forEach(function (element) {
+    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
+    const locale = Intl.DateTimeFormat().resolvedOptions().locale
+    const datetimeString = element.getAttribute('data-datetime')
+    const datetimeObj = new Date(datetimeString)
     const localDate = datetimeObj.toLocaleString(locale, {
       timezone: tz,
-      timeZoneName: "short",
-    });
-    // convert from "DD/MM/YYYY, HH:MM:SS TZ" to "YYYY-MM-DD HH:MM TZ"
+      timeZoneName: 'short',
+    })
+    // convert from 'DD/MM/YYYY, HH:MM:SS TZ' to 'YYYY-MM-DD HH:MM TZ'
     const displayDate = localDate.replace(
       /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}:\d{2}):\d{2} (\w+)/,
-      "$3-$2-$1 $4 $5"
-    );
-    element.textContent = displayDate;
-  });
-});
+      '$3-$2-$1 $4 $5'
+    )
+    element.textContent = displayDate
+  })
+})
