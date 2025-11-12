d2cfbaad6cfe7b517d223bf86ca32362ae682e4e
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-11-12 16:52:36 +0000

    Log on run_celery

--- run_celery.py
+++ run_celery.py
@@ -1,4 +1,9 @@
+import logging
+
 from app import create_app, notify_celery  # noqa
 
 application = create_app()
 application.app_context().push()
+
+logger = logging.getLogger(__name__)
+logger.info("run_celery init")
