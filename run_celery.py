520145de8332bb9291005c2ae9745930bf162235
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-11-13 11:51:17 +0000

    Add autoinstrument to run_celery?

--- run_celery.py
+++ run_celery.py
@@ -1,5 +1,9 @@
 import logging
 
+from opentelemetry.instrumentation.auto_instrumentation import initialize
+
+initialize()
+
 from app import create_app, notify_celery  # noqa
 
 application = create_app()
