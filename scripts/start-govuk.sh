d37b2881334bf37f7b3b1c13bc8e2e571d6d65e9
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-07-16 08:43:30 +0100

    Add DEBUGPY_PORT to startup script

--- scripts/start-govuk.sh
+++ scripts/start-govuk.sh
@@ -6,14 +6,20 @@ function configure_container_role(){
     aws configure set default.region ${AWS_REGION:-eu-west-2}
 }
 
+PYTHON_COMMAND="python -m"
+if [[ ! -z $DEBUGPY_PORT ]]; then
+    echo "Starting with debugpy on port $DEBUGPY_PORT"
+    PYTHON_COMMAND="python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:${DEBUGPY_PORT} -m"
+fi
+
 function run_dramatiq(){
     cd $DIR_GOVUK;
-    . $VENV_GOVUK/bin/activate && exec dramatiq --skip-logging --processes 1 --threads 2 app.dramatiq_broker:broker
+    . $VENV_GOVUK/bin/activate && exec $PYTHON_COMMAND dramatiq --skip-logging --processes 1 --threads 2 app.dramatiq_broker:broker
 }
 
 function flask_publish(){
     cd $DIR_GOVUK;
-    . $VENV_GOVUK/bin/activate && flask publish-with-assets --startup
+    . $VENV_GOVUK/bin/activate && $PYTHON_COMMAND flask publish-with-assets --startup
 }
 
 function update_timestamp(){
